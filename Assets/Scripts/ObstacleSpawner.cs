85f744bf44c7b0cfdf64c91c07bb0feed2578e4c
aldsanms <aldryckgta@gmail.com>
2025-07-17 18:36:49 +0200

    add random obtacles offset

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -7,12 +7,15 @@ public class ObstacleSpawner : MonoBehaviour
     public GameObject obstaclePrefab;
     public float spawnRate = 2f;
     public float spawnHeight = 2f;
+    public float verticalVariation = 2f;  
     private float timer = 0f;
 
     void Update() {
         timer += Time.deltaTime;
         if (timer >= spawnRate) {
-            Instantiate(obstaclePrefab, new Vector3(10f, spawnHeight, 0), Quaternion.identity);
+            float randomYOffset = Random.Range(-verticalVariation, verticalVariation);
+            
+            Instantiate(obstaclePrefab, new Vector3(20f +randomYOffset, spawnHeight, 0), Quaternion.identity);
             timer = 0f;
         }
     }
